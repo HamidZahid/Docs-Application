edf5581de1bd05e3419597fdc15a9d89307689c9
HamidZahid <hamidchaudhary3867@gmail.com>
2025-04-28 17:03:59 +0500

    Project Completed

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,24 +2,28 @@ import React from "react";
 import { FaFileCsv } from "react-icons/fa";
 import { IoIosCloudDownload } from "react-icons/io";
 import { FaDownload } from "react-icons/fa";
+import { IoIosClose } from "react-icons/io";
 
-
-function Card() {
+function Card({ data }) {
   return (
-    <div className="relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white py-10 px-8 overflow-hidden">
+    <div className="relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white py-10 px-8 overflow-hidden">
       <FaFileCsv />
       <p className="mt-4 font-semibold text-sm leading-tight">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
-        molestiae ipsum ex mollitia voluptates doloribus?
+        {data.desc}
       </p>
       <div className="footer absolute w-full  bottom-0 left-0 ">
         <div className="flex justify-between mb-1 px-8 py-2">
-            <h5>0.4mb</h5>
-            <FaDownload />
+          <h5>{data.fileSize}</h5>
+            <span>
+                {data.close ? <IoIosClose /> : <FaDownload />}
+            </span>
+          
         </div>
-        <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-            <h3 className="text-sm font-semibold">Download Now</h3>
-        </div>
+        {data.tag.isOpen && (
+            <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
+            <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+          </div>
+        )}  
       </div>
     </div>
   );
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
 function Foreground() {
+  const data = [
+    {
+      desc: "This Is The Context You wanna See innthe profilde .....",
+      fileSize: ".9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "This Is The Context You wanna See innthe profilde .....",
+      fileSize: ".9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "This Is The Context You wanna See in the profile of your favourite star and other Fans.",
+      fileSize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Cancel Now", tagColor: "red" },
+    },
+  ];
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
-        <div className='flex items-center justify-center '>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-        </div>
-        
+    <div className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5 ">
+      {data.map((item, index) => (
+        <Card data={item} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Foreground
+export default Foreground;
