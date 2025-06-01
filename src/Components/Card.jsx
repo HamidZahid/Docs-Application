847c6a8dcd9e9a699c61f02266f8117dbfcb3f3e
M_Hamid_Zahid <85985817+HamidZahid@users.noreply.github.com>
2025-06-01 14:36:37 +0500

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,7 +8,7 @@ function Card({ data }) {
   return (
     <div className="relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white py-10 px-8 overflow-hidden">
       <FaFileCsv />
-      <p className="mt-6 font-semibold text-sm leading-tight">
+      <p className="mt-4 font-semibold text-sm leading-tight">
         {data.desc}
       </p>
       <div className="footer absolute w-full  bottom-0 left-0 ">
